ec5e51f70e7beb5b2fde03fce5382915ca3d6220
bbbboraa <purple2851@naver.com>
2023-05-02 22:32:32 +0900

    달력 생성

--- app/src/main/java/com/example/seoulo1/MainActivity.java
+++ app/src/main/java/com/example/seoulo1/MainActivity.java
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_calenderOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_calenderOK = findViewById(R.id.btn_calenderOK);
+        btn_calenderOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
     }
-    //푸시 받아라!!!!!
 
 }
