1a0d3a47709f5d32c30929d9929fefef90f10347
bbbboraa <purple2851@naver.com>
2023-05-03 17:21:42 +0900

    이하은을 위한것

--- app/src/main/java/com/example/seoulo1/MainActivity.java
+++ app/src/main/java/com/example/seoulo1/MainActivity.java
@@ -1,31 +1,17 @@
 package com.example.seoulo1;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button btn_calenderOK;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btn_calenderOK = findViewById(R.id.btn_calenderOK);
-        btn_calenderOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
-
     }
-
 }
