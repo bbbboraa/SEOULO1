35f2f7e2c6a1a040dd643b8d1def9a8c86cf4ebf
박시현 <gkrry1117@gmail.com>
2023-05-03 20:05:04 +0900

    수정

--- app/src/main/java/com/example/seoulo1/MainActivity.java
+++ app/src/main/java/com/example/seoulo1/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.seoulo1;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_calenderOK;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_calenderOK = findViewById(R.id.btn_calenderOK);
+        btn_calenderOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
     //푸시 받아라!!!!!
     //하은 하이 줄여서 하하
