07d5a3e7cd67ff1a58ad3070dda2666dc6642808
박시현 <gkrry1117@gmail.com>
2023-05-11 12:46:06 +0900

    비밀번호 재설정

--- app/src/main/java/com/example/seoulo1/ResetPasswordActivity.java
+++ app/src/main/java/com/example/seoulo1/ResetPasswordActivity.java
@@ -1,14 +1,53 @@
 package com.example.seoulo1;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ResetPasswordActivity extends AppCompatActivity {
 
+    EditText send_email;
+    Button btn_reset;
+
+    FirebaseAuth auth;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        send_email = findViewById(R.id.send_email);
+        btn_reset = findViewById(R.id.btn_reset);
+
+        auth = FirebaseAuth.getInstance();
+
+        btn_reset.setOnClickListener((View.OnClickListener) v -> {
+            //email 보내기
+            String email = send_email.getText().toString();
+
+            if(email.equals("")){
+                Toast.makeText(ResetPasswordActivity.this, "이메일을 입력해주세요!", Toast.LENGTH_SHORT).show();
+            } else {
+                auth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+                    if(task.isSuccessful()) {
+                        Toast.makeText(ResetPasswordActivity.this, "메일함을 확인해주세요!", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                    } else {
+                        String error = task.getException().getMessage();
+                        Toast.makeText(ResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
     }
 }
