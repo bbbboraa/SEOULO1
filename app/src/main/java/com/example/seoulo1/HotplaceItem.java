748fc3d7cfa97bbe13380c12f0c6126a6fcadbd7
하은 <lhe.010223@daum.net>
2023-11-15 18:55:02 +0900

    +1115 중간 푸쉬

--- app/src/main/java/com/example/seoulo1/HotplaceItem.java
+++ app/src/main/java/com/example/seoulo1/HotplaceItem.java
@@ -2,12 +2,18 @@ package com.example.seoulo1;
 
 public class HotplaceItem {
     private String itemName;
+    private String naverUrl;
 
-    public HotplaceItem(String itemName) {
+    public HotplaceItem(String itemName, String naverUrl) {
         this.itemName = itemName;
+        this.naverUrl = naverUrl;
     }
 
     public String getItemName() {
         return itemName;
+    }
+
+    public String getNaverUrl() {
+        return naverUrl;
     }
 }
